2f03fbfaacd5ca1793f21b57bbacdca6148ce610
Gil Caplan <74542358+GilCaplan@users.noreply.github.com>
2021-12-03 15:09:19 +0200

    Update day1solution.java

--- Javasolutions/day1solution.java
+++ Javasolutions/day1solution.java
@@ -1,20 +1,23 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class day1solution {		
 	public static Scanner reader= new Scanner(System.in);
 	public static void main(String args[]){
 		List<Integer> input = new ArrayList<Integer>();
-		int i = reader.nextInt(), cnt=0;
-		while(i != -1) {
-			input.add(i);
-			i = reader.nextInt();
+		String s="";
+		File inputFile = new File("/C:/Users/avaca/eclipse-workspace/Advent2021/day1_input.txt");
+		Scanner scan;
+		try {
+			scan = new Scanner(inputFile);
+			while(scan.hasNext()) {
+				s = scan.nextLine();
+				input.add(Integer.valueOf(s));
+			}
+			System.out.println("input received");
+			System.out.println(part1(input));
+			System.out.println(part2(input));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println(part1(input));
-		System.out.println(part2(input));
 	}
 	public static int part1(List<Integer> input) {
 		int cnt = 0, i=0;
@@ -36,5 +39,6 @@ public class day1solution {
 		return cnt;
 	}
 }
+
 
 
